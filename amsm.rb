9e558fa8b7dba405c86e0004001afd568f4aa880
Buzzb0mb <Buzzb0mb@github.com>
2020-03-21 23:37:58 -0400

    Fix indentation. Clean up functions. Add comments.

--- amsm.rb
+++ amsm.rb
@@ -6,12 +6,12 @@
 
 class Session
 	def initialize(name, exec)
-			@name = name
-			@exec = exec
+		@name = name
+		@exec = exec
 	end
 
 	def call
-			system "startx #{@exec}"
+		system "startx #{@exec}"
 	end
 
 	attr_reader :name
@@ -34,30 +34,38 @@ def parse_session(data)
 	return nil if not lines[0] == '[Desktop Entry]'
 	lines.shift
 
-	options = lines.map{|l| (l=l.split '=').length == 2 ? l : (return nil)}.to_h
-	return Session.new((options['Name'] or return nil), (options['Exec'] or return nil))
+	options = lines
+		.select{|l| l.count('=') == 1} # only keep lines that have an '=' so a split('=') will yield 2 elements
+		.map{|l| l.split('=')}         # split ["key=value"] into [["key", "value"]]
+		.to_h                          # convert [["key", "value"]] into {"key" => "value"}
+	return nil if options['Name'].nil? or options['Exec'].nil?
+	return Session.new(options['Name'], options['Exec'])
 end
 
 def read_sessions
-	invalid_lambda = lambda { puts 'Invalid index'}	
-	sessions = Dir.glob('/usr/share/xsessions/*.desktop').map.with_index(1) do |path, index|
-		[index, parse_session(File.read(path))]
-	end.reject{|session| session[1].nil?}.to_h
-	sessions.default = invalid_lambda
+	sessions = Dir.glob('/usr/share/xsessions/*.desktop')
+		.map{|p| parse_session(File.read(p))} # parse the session file at each path
+		.reject(&:nil?)     # remove invalid sessions
+		.each.with_index(1) # iterate through each object with its index starting at 1 instead of 0
+		.to_a               # convert to an array like [[session1, 1], [session2, 2]]
+		.map(&:reverse)     # reverse each element of the array into [[1, session1], [2, session2]]
+		.to_h               # convert into a hash like {1 => session1, 2 => session2}
+	sessions.default = lambda { puts 'Invalid index' }
 	sessions
 end
 
 def print_sessions(session_array)
-	session_array.each_pair do |index, session|
-		puts "  #{index} #{session.name}"
-	end
+	puts session_array
+		.map{|index, session| "  #{index} #{session.name}"}
+		.join("\n")
 end
 
 def prompt(sessions, commands)
 	loop do
 		print 'amsm> '
-		argv = $stdin.gets.chomp.split ' '
-		commands[commands.keys.select { |regex| argv[0].match regex }[0]][argv, sessions]
+		(argv = $stdin.gets.chomp.split(' ')).empty? and next
+		r = commands.find(->{0}) {|regex, func| argv[0].match(regex)}[0]
+		commands[r][argv, sessions]
 	end
 end
 
